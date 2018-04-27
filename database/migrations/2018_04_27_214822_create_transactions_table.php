c499ee0e27f4e6cdc18253166d8bf0c745855de7
Sambhu <rajsamb@hotmail.com>
2018-04-27 22:52:30 +0100

    Create db migration for the stores and transactions

---
+++ database/migrations/2018_04_27_214822_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('transaction_id');
+            $table->integer('store_id')->unsigned();
+            $table->decimal('total_amount', 5, 2);
+            $table->enum('currency', ['GBP', 'USD', 'EUR']);
+            $table->dateTime('created_at');
+        });
+
+        Schema::table('transactions', function($table) {
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
