ff1eaf2ce3412ea479b99f8abd8e386d4e27f9bd
Sambhu <rajsamb@hotmail.com>
2018-04-28 21:14:55 +0100

    Set mass assignment field

--- app/Store.php
+++ app/Store.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     /**
+     * Timestamp not used.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'name', 'address', 'postcode'];
+
+    /**
      * A particular store can have many transactions.
      */
     public function transaction()
