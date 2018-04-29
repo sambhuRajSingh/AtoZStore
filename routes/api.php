fe9110f0a9a7a089799567cae32864c47478e8fb
Sambhu <rajsamb@hotmail.com>
2018-04-29 18:55:14 +0100

    Create route to Transaction and Transaction Refund Controller.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'API'],
+    function () {
+        Route::get('transactions', 'TransactionController@index');
+        Route::post('transactions', 'TransactionController@store');
+
+        Route::get('refundTransaction', 'TransactionRefundController@index');
+        Route::post('refundTransaction', 'TransactionRefundController@store');
+    });
+
